82f78d1df14854b714a023f5c70c2f9eaee5ef1f
Anastasia Mik <mikunova1108@gmail.com>
2024-07-07 22:52:00 +0300

    Refactor UserController with EntityManager

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -27,43 +30,71 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
-    // удаление пользователя
-    #[Route('/delete/{id}', name: 'user_delete', methods: ['DELETE'])]
-    public function deleteUser(string $id): Response
-    {
-//        $em = $this->getDoctrine()->getManager();
-//        $user = $em->getRepository(User::class)->find($id);
-//
-//        if (!$user) {
-//            throw $this->createNotFoundException(
-//                'No user found for id '.$id
-//            );
-//        }
-//
-//        $em->remove($user);
-//        $em->flush();
 
-        return new Response('User deleted');
-    }
-    #[Route('/change/{id}', name: 'user_change', methods: ['PUT'])]
-    public function changeUser (string $id): Response
+    // удаление пользователя
+    #[Route('/{id}', name: 'user_delete', methods: ['DELETE'])]
+    public function deleteUser(string $id, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $repository = $entityManager->getRepository(User::class);
+
+        $user = $repository->find($id);
+
+        if ($user) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            return new Response('User deleted');
+        }
+
+        throw $this->createNotFoundException(
+            'No user found for id '.$id
+        );
     }
-    #[Route('/edit/{id}', name: 'user_edit', methods: ['PATCH'])]
-    public function editUser (string $id): Response
+    #[Route('/{id}', name: 'user_change', methods: ['PUT'])]
+    public function changeUser(string $id, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $repository = $entityManager->getRepository(User::class);
+        $user = $repository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if(isset($data['name'])){
+            $user->setName($data['name']);
+        }
+        if(isset($data['surname'])){
+            $user->setSurname($data['surname']);
+        }
+        if(isset($data['email'])){
+            $user->setEmail($data['email']);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'User updated'], Response::HTTP_OK);
     }
+
     #[Route('/', name: 'user_create', methods: ['POST'])]
-    public function createUser(Request $request): Response
+    public function createUser(Request $request, EntityManagerInterface $em): JsonResponse
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-            ]);
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['name']) || !isset($data['surname']) || !isset($data['email'])) {
+            return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = new User();
+        $user->setName($data['name']);
+        $user->setSurname($data['surname']);
+        $user->setEmail($data['email']);
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['status' => 'User created'], Response::HTTP_CREATED);
     }
 }
